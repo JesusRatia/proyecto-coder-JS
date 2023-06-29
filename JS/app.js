dbf2a8663a8e9f5f9c5786a1ba69df8ced85668d
JesusRatia <131001567+JesusRatia@users.noreply.github.com>
2023-06-29 14:26:57 -0400

    guardado la segunda pre-entrega

--- JS/app.js
+++ JS/app.js
@@ -1,21 +1,38 @@
-    let operador = prompt(" que operacion deseas haccer : 1 - Sumar || 2 - Restar || 3 - multiplicar || 4 - dividir");
-    let numero1 = Number(prompt(" ingrese la primera operacion"));
-    let numero2 = Number(prompt("ingrese la segunda oepracion"));
-    let total;
+const tasasDeCambio = {
+  USD: {
+    EUR: 0.90,
+    BSV: 29.5,
+    COP: 5000
+  },
+  EUR: {
+    USD: 1.18,
+    BSV: 29.5,
+    COP: 5000
+  },
+  BSV: {
+    USD: 29.5,
+    EUR: 29,
+    COP: 151.49
+  },
+  COP: {
+    USD: 4.177,
+    EUR: 4.565,
+    BSV: 151
+  }
+};
 
-if (operador == 1){
-    total = numero1 + numero2;
-    alert("el resultado de la suma es de"+ total);
-}else if(operador == 2){
-    total = numero1 - numero2;
-    alert("el tresultado de la resta es de"+total);
-}else if(operador == 3){
-    total = numero1 * numero2;
-    alert("el resultado de la multiplicacion es de"+total);
-}else if(operador == 4){
-    total = numero1 / numero2;
-    alert("el resultado de la division es de"+total);
-}else{
-    alert("operacion incorrecta")
+function convertirDivisa() {
+  const cantidad = parseFloat(prompt('Ingrese la cantidad a convertir:'));
+  const divisaOrigen = prompt('Ingrese la divisa de origen (USD, EUR, BSV, COP):');
+  const divisaDestino = prompt('Ingrese la divisa de destino (USD, EUR, BSV, COP):');
+
+  if (!tasasDeCambio.hasOwnProperty(divisaOrigen) || !tasasDeCambio[divisaOrigen].hasOwnProperty(divisaDestino)) {
+    alert('No se encontró una tasa de cambio para la conversión especificada.');
+    return;
+  }
+
+  const tasaDeCambio = tasasDeCambio[divisaOrigen][divisaDestino];
+  const cantidadConvertida = cantidad * tasaDeCambio;
+
+  alert(cantidad + ' ' + divisaOrigen + ' equivale a ' + cantidadConvertida + ' ' + divisaDestino);
 }
-
